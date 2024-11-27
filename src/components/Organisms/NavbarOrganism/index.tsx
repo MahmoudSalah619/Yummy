f95d7471769e878f6a032fba0dcac4113d77603b
Mohamed Ayman <mohamed.aymann944@gmail.com>
2024-11-27 16:46:28 +0200

    finished notification overlay

--- src/components/Organisms/NavbarOrganism/index.tsx
+++ src/components/Organisms/NavbarOrganism/index.tsx
@@ -1,10 +1,13 @@
 import NavbarLink from "../../Molecules/NavbarLink";
+import NavbarNotification from "../../Molecules/NavbarNotification";
 import styles from "./styles.module.scss";
 
 function NavbarOrganism() {
   return (
     <nav className={styles.navbar}>
       <NavbarLink />
+
+      <NavbarNotification />
     </nav>
   );
 }
