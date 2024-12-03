5e5ae2dffd1ede628f89145700099edc47551562
abdelfattahSalem <bedosalm122@gmail.com>
2024-12-04 00:48:38 +0200

    Contact Us Done

---
+++ src/components/Organisms/ContactUs/index.tsx
@@ -0,0 +1,52 @@
+import { Select } from "antd";
+import styles from "./styles.module.scss";
+import Text from "../../Atoms/Text";
+import TextInput from "../../Atoms/TextInput";
+import Button from "../../Atoms/Button";
+
+function ContactUs() {
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+  return (
+    <div className={styles.container}>
+      <Text className={styles.MainTitle} fontSize={30}>
+        Contact Us
+      </Text>
+      <div className={styles.contactUs}>
+        <Text className={styles.title} fontFamily="font700" fontSize={18}>
+          We’d love to hear from you! Feel free to reach out with any questions
+          or feedback.
+        </Text>
+
+        <div className={styles.SelectInputContent}>
+          <span className="d-block mb-2 Label100 White">Subject</span>
+          <Select
+            className={styles.SelectInput}
+            style={{ width: "100%" }}
+            onChange={handleChange}
+            options={[
+              { value: "jack", label: "Jack" },
+              { value: "lucy", label: "Lucy" },
+              { value: "Yiminghe", label: "yiminghe" },
+            ]}
+          />
+        </div>
+        <TextInput
+          type="textarea"
+          label="Message"
+          containerStyle={styles.textArea}
+        />
+        <div className={styles.SubmitInfo}>
+          <Button title="Submit" customStyle={styles.btn} />
+          <Text>Customer Support Info</Text>
+          <Text>
+            Email: <span> support@ardi.com</span>
+          </Text>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactUs;
