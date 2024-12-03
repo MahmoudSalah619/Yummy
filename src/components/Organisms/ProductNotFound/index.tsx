be69bf7c19e596094439a30a61aeaf139418363b
abdelfattahSalem <bedosalm122@gmail.com>
2024-12-03 21:18:46 +0200

    Product not found Done

--- src/components/Organisms/ProductNotFound/index.tsx
+++ src/components/Organisms/ProductNotFound/index.tsx
@@ -1,15 +1,16 @@
 import styles from "./styles.module.scss";
 import Image from "../../Atoms/Image";
-import NotFoundProduct from "@/src/assets/icons/NotFoundProduct.svg";
 import Text from "../../Atoms/Text";
+import Button from "../../Atoms/Button";
+import { ProductNotFoundItems } from "./types";
 
-function ProductNotFound() {
+function ProductNotFound({ title, description, image }: ProductNotFoundItems) {
   return (
     <div className={styles.NotFoundContent}>
-      <Image src={NotFoundProduct} alt="hallow" width={100} height={100} />
-      <Text fontFamily="font500">You have no products yet</Text>
-      <Text>When you add products they will appear here.</Text>
-      <button>Button</button>
+      <Image src={image} alt="hallow" width={100} height={100} />
+      <Text className={styles.boldText}>{title}</Text>
+      <Text>{description}</Text>
+      <Button title="Add Product" />
     </div>
   );
 }
