ef8959e273044e1b715c148e366653bea5f6ed94
abdelfattahSalem <bedosalm122@gmail.com>
2024-12-08 11:25:59 +0200

    end Order Timeline

--- src/components/Organisms/OrderTimeline/index.tsx
+++ src/components/Organisms/OrderTimeline/index.tsx
@@ -1,30 +1,46 @@
 import Text from "../../Atoms/Text";
 import CardWrapper from "../../Wrappers/CardWrapper";
 import styles from "./styles.module.scss";
-import { OrderTimelineStatus } from "./types";
+// import { OrderTimelineStatus } from "./types";
 
-function OrderTimeline({ status, date }: OrderTimelineStatus) {
+function OrderTimeline() {
+  const OrderTimeLineArray = [
+    {
+      process: "Order Delivered",
+      status: "Done",
+      date: "27/5/2024",
+    },
+    {
+      process: "Order Shipped",
+      status: "Done",
+      date: "27/5/2024",
+    },
+    {
+      process: "Order Accepted",
+      status: "pending",
+      date: "27/5/2024",
+    },
+    {
+      process: "Order Placed",
+      status: "pending",
+      date: "27/5/2024",
+    },
+  ];
+
   return (
     <CardWrapper>
       <div className={styles.OrderTimelineContent}>
         <Text className={styles.title}>Order Timeline</Text>
-
-        <div className={styles.Timeline}>
-          <Text>{status}</Text>
-          <Text>{date}</Text>
-        </div>
-        <div className={styles.Timeline}>
-          <Text>{status}</Text>
-          <Text>{date}</Text>
-        </div>
-        <div className={styles.Timeline}>
-          <Text>{status}</Text>
-          <Text>{date}</Text>
-        </div>
-        <div className={styles.Timeline}>
-          <Text>{status}</Text>
-          <Text>{date}</Text>
-        </div>
+        {OrderTimeLineArray.map((item) => {
+          return (
+            <div
+              className={`${styles.Timeline} ${item.status === "Done" ? styles.Done : styles.pending}`}
+            >
+              <Text>{item.process}</Text>
+              <Text>{item.date}</Text>
+            </div>
+          );
+        })}
       </div>
     </CardWrapper>
   );
