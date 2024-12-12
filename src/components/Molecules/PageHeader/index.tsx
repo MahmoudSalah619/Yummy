ed19b4c39550a67bbae20864d76686e6c36e91da
Omar Tamer <omartamergado@gmail.com>
2024-12-12 11:08:10 +0200

    enhancment the subCategories code

--- src/components/Molecules/PageHeader/index.tsx
+++ src/components/Molecules/PageHeader/index.tsx
@@ -5,13 +5,15 @@ import styles from "./styles.module.scss";
 function PageHeader({
   title,
   children,
+  fontSize = 36,
 }: {
   title: string;
   children?: ReactNode;
+  fontSize?: number;
 }) {
   return (
     <div className={styles.container}>
-      <Text fontSize={36} fontFamily="font500" color="grey900">
+      <Text fontSize={fontSize} fontFamily="font500" color="grey900">
         {title}
       </Text>
       <div className={styles.childrenContainer}>{children}</div>
--- src/components/Organisms/SubCategories/index.tsx
+++ src/components/Organisms/SubCategories/index.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import styles from "./styles.module.scss";
 import CardWrapper from "../../Wrappers/CardWrapper";
-import Text from "../../Atoms/Text";
-import Button from "../../Atoms/Button";
 import SubCategory from "../../Molecules/SubCategory";
 import CustomPagination from "../../Molecules/CustomPagination";
-import icon from "../../../assets/icons/navbar/notification-icon.svg";
-import Image from "../../Atoms/Image";
+import PageHeader from "../../Molecules/PageHeader";
+import FilterOrganism from "../FilterOrganism";
 
 function SubCategories() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -26,14 +24,10 @@ function SubCategories() {
   return (
     <CardWrapper>
       <div className={styles.SubCategoriesContent}>
-        <div className={styles.SubCategoriesHeader}>
-          <Text className={styles.titleStyle} fontSize={21}>
-            Sub Categories
-          </Text>
-          <div className={styles.FiltersBtn}>
-            <Image src={icon} width={20} height={20} alt="filters" />
-            <Button color="dark" title="Filters" customStyle={styles.btn} />
-          </div>
+        <div className={styles.headerContaier}>
+          <PageHeader fontSize={24} title="Sub Categories">
+            <FilterOrganism />
+          </PageHeader>
         </div>
         <div className={styles.categoriesList}>
           {categories.map((item) => {
