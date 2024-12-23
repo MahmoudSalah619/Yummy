c16654f52b4a94c10a03d8e270383a74baefea2e
Mohamed Ayman <mohamed.aymann944@gmail.com>
2024-12-23 16:04:29 +0200

    fixed image in order details in higher res (1920x1080) / ARDI-155

--- src/components/Organisms/OrderItemsCard/index.tsx
+++ src/components/Organisms/OrderItemsCard/index.tsx
@@ -6,15 +6,17 @@ import Text from "../../Atoms/Text";
 function OrderItemsCard() {
   return (
     <div className={styles.ProductCardContent}>
-      <div className={styles.ImageContent}>
-        <Image src={item} width={100} height={100} alt="T-shirt" />
-      </div>
-      <div className={styles.FirstLine}>
-        <Text>Te-shirt</Text>
-        <Text color="grey200" className={styles.brandName}>
-          BrandName
-        </Text>
-        <Text>m / red</Text>
+      <div className={styles.ProductCard}>
+        <div className={styles.ImageContent}>
+          <Image src={item} width={100} height={100} alt="T-shirt" />
+        </div>
+        <div className={styles.FirstLine}>
+          <Text>Te-shirt</Text>
+          <Text color="grey200" className={styles.brandName}>
+            BrandName
+          </Text>
+          <Text>m / red</Text>
+        </div>
       </div>
       <Text>EGP : 1000 * 1 </Text>
       <Text>EGP 1000 </Text>
